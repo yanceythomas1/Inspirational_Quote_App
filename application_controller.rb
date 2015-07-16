5d42d32edd22e03f8bae0440253df519229ef501
Yancey Thomas <ythomas@leaders6-12.org>
2015-07-16 15:31:27 +0000

    Sports Page Processing Properly

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,21 @@
 require 'bundler'
 Bundler.require
-
+require_relative "models/model.rb"
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+  get '/sports' do
+    erb :sports
+  end
+  post '/quote' do
+    puts params
+    @quote1 = Quote.new(params["category"],params["answer"])
+    puts @quote1 
+    puts @quote1.category
+    puts @quote1.answer
+    erb :quote
   end
 
 end
---
+++ models/model.rb
@@ -0,0 +1,7 @@
+class Quote 
+  attr_accessor :category, :answer
+  def initialize(category, answer)
+    @category = category
+    @answer = answer
+  end 
+end 
